d179add664638ff259a0b49bab2dfc92b6d23116
Dylan Comeau <comeaudc@comcast.net>
2022-09-08 11:57:47 -0400

    Added Edit Exercise Page and Button

--- src/services/tracker-api.js
+++ src/services/tracker-api.js
@@ -36,6 +36,12 @@ export const createWorkout = (workout) => {
     return response
 }
 
+export const editExercise = (id, updatedExerise) => {
+    const URL = `https://trackerbackenddcc.herokuapp.com/tracker/exercises/${id}`
+    const response = axios.put(URL, updatedExerise)
+    return response
+}
+
 export const editWorkout = (id, updatedWorkout) => {
     const URL = `https://trackerbackenddcc.herokuapp.com/tracker/history/${id}`
     const response = axios.put(URL, updatedWorkout)
